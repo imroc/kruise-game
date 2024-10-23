d33fb7b921087ecb3934f09e9f7a432c93a48be9
roc <roc@imroc.cc>
2024-10-23 20:13:22 +0800

    update at 2024-10-23 20:13:22

--- cloudprovider/options/tencentcloud_options.go
+++ cloudprovider/options/tencentcloud_options.go
@@ -1,25 +1,25 @@
 package options
 
 type TencentCloudOptions struct {
-	Enable     bool                   `toml:"enable"`
-	CLBOptions TencentCloudCLBOptions `toml:"clb"`
+	Enable bool `toml:"enable"`
+	// CLBOptions TencentCloudCLBOptions `toml:"clb"`
 }
 
-type TencentCloudCLBOptions struct {
-	MaxPort int32 `toml:"max_port"`
-	MinPort int32 `toml:"min_port"`
-}
+// type TencentCloudCLBOptions struct {
+// 	MaxPort int32 `toml:"max_port"`
+// 	MinPort int32 `toml:"min_port"`
+// }
 
 func (o TencentCloudOptions) Valid() bool {
-	clbOptions := o.CLBOptions
-
-	if clbOptions.MaxPort > 65535 {
-		return false
-	}
-
-	if clbOptions.MinPort < 1 {
-		return false
-	}
+	// clbOptions := o.CLBOptions
+	//
+	// if clbOptions.MaxPort > 65535 {
+	// 	return false
+	// }
+	//
+	// if clbOptions.MinPort < 1 {
+	// 	return false
+	// }
 
 	return true
 }
